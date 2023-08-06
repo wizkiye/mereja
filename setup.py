db91c34c44cf0cf90d9e5c8134408799ca525f65
wizkiye <wizkiye@gmail.com>
2023-08-06 19:49:50 +0300

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename: str):
 
 setup(
     name="mereja",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     url="https://github.com/wizkiye/mereja",
     license="MIT",
